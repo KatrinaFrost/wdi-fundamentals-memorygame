bd09997ec99def7afe05f5fd61ff7f7a586b8f0b
KatrinaFrost <katrinalfrost@hotmail.com>
2017-07-10 21:00:06 +1000

    Added JS arrays and if else statements

--- MemoryFlash/js/main.js
+++ MemoryFlash/js/main.js
@@ -1,7 +1,34 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour= "king";
+console.log("Up and running!");
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+//var cardOne = "queen";
+//var cardTwo = "king";
+//var cardThree = "queen";
+//var cardFour = "king"; unit 7 assignment
+
+console.log("user flipped" + cardOne);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("user flipped " + cardOne);
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+console.log("user flipped " + cardTwo);
+
+
+
+if (cardsInPlay.length === 2) //if statement
+{
+  console.log("you have selected two cards");
+
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+     alert("You found a match!");
+   } else {
+     alert("Sorry, try again");
+   }
+}
